05ffdd4a4befef37b6128cf4bf92335d6ff9edb5
Matteo Vaccari <mvaccari@thoughtworks.com>
2020-10-22 18:04:13 +0200

    We can read a property file

--- src/main/java/it/xpug/simple_config/SimpleConfig.java
+++ src/main/java/it/xpug/simple_config/SimpleConfig.java
@@ -1,4 +1,26 @@
 package it.xpug.simple_config;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class SimpleConfig {
+    private Properties properties = new Properties();
+
+    public SimpleConfig() {
+    }
+
+    public String get(String propertyName) {
+        return (String) properties.get(propertyName);
+    }
+
+    public SimpleConfig load(Path path) throws IOException {
+        BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset());
+        properties.load(reader);
+        return this;
+    }
 }
--- src/test/java/it/xpug/simple_config/LibraryTest.java
+++ src/test/java/it/xpug/simple_config/LibraryTest.java
@@ -4,11 +4,18 @@
 package it.xpug.simple_config;
 
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     @Test
     void testSomeLibraryMethod() throws Exception {
-        SimpleConfig simpleConfig = new SimpleConfig();
+        SimpleConfig simpleConfig = new SimpleConfig().load(Paths.get(System.getProperty("user.dir"), "fixture", "application.properties"));
+
+        assertThat(simpleConfig.get("foo.bar")).isEqualTo("baz");
     }
 }
